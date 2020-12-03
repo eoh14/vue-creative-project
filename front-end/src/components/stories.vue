bbe04c54f00c196528a6c30cc4d9a7f001effb44
Eriene Oh <erieneoh@hotmail.com>
2020-12-03 15:27:38 -0700

    try again?

--- front-end/src/components/stories.vue
+++ front-end/src/components/stories.vue
@@ -36,7 +36,7 @@ export default {
         async getStories() {
         try {
             let response = await axios.get("/api/stories");
-            this.astory = response.data.astory;
+            this.astory = response.data.story;
             return true;
         } catch (error) {
             console.log(error);
