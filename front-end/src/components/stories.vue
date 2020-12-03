a9e23aca47a6fd7f87578379916dbc6cd17fd2b4
Eriene Oh <erieneoh@hotmail.com>
2020-12-03 15:37:24 -0700

    another update global

--- front-end/src/components/stories.vue
+++ front-end/src/components/stories.vue
@@ -2,8 +2,8 @@
     
     <div> 
     <div class="container border my-2 p-3 text-center">
-        <h4>{{astory.story}}</h4>
-        <h6 class="d-flex justify-content-center text-muted"> -- {{astory.name}}</h6>
+        <h4>{{this.$root.$data.chosenStory}}</h4>
+        <h6 class="d-flex justify-content-center text-muted"> -- {{this.$root.$data.chosenAuthor}}</h6>
     </div>
         <button @click="getStories " id="goQuotes" class="btn btn-light"> Storytime? </button>
     <div>
@@ -37,6 +37,8 @@ export default {
         try {
             let response = await axios.get("/api/stories");
             this.astory = response.data.story;
+            this.$root.$data.chosenStory = this.astory.story;
+            this.$root.$data.chosenAuthor = this.astory.name;
             return true;
         } catch (error) {
             console.log(error);
